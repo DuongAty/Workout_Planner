160dcf00af4eca3cf03eb50235cc530c5260dd97
DuongAty <atyduongtran@gmail.com>
2025-12-24 11:36:46 +0700

    fix(auth): fix bug in getUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Logger, Post, Get } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AccessTokenPayload } from './type/accessToken.type';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
   private logger = new Logger('AuthController');
@@ -19,8 +22,9 @@ export class AuthController {
     return this.authService.signIn(authCredentialsDto);
   }
   @Get('/me')
-  async getMe(@Headers('authorization') authHeader: string) {
-    const token = authHeader?.split(' ')[1];
-    return this.authService.getUser(token);
+  @ApiBearerAuth('accessToken')
+  @UseGuards(AuthGuard())
+  async getMe(@Req() req) {
+    return await req.user;
   }
 }
