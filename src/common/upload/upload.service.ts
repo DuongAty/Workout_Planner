f9c0b81f9baae71a379fbacc3e1b53df1f4792b2
DuongAty <atyduongtran@gmail.com>
2025-12-30 09:19:44 +0700

    fix(): fix some error

--- src/common/upload/upload.service.ts
+++ src/common/upload/upload.service.ts
@@ -16,15 +16,12 @@ export class UploadService {
 
   cloneFile(relativeOldPath: string): string | null {
     if (!relativeOldPath || !fs.existsSync(`./${relativeOldPath}`)) return null;
-
     const ext = path.extname(relativeOldPath);
     const fileName = path.basename(relativeOldPath, ext);
     const directory = path.dirname(relativeOldPath);
-
     const newRelativePath = path
       .join(directory, `${fileName}-clone-${Date.now()}${ext}`)
       .replace(/\\/g, '/');
-
     fs.copyFileSync(`./${relativeOldPath}`, `./${newRelativePath}`);
     return newRelativePath;
   }
