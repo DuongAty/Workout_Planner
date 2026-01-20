b8d0f5ef98e727c7c04b868aef20c9eb762851cf
DuongAty <atyduongtran@gmail.com>
2026-01-20 09:49:36 +0700

    fix(): fix some errors

--- src/workoutplan/workoutplan.service.ts
+++ src/workoutplan/workoutplan.service.ts
@@ -1,7 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, EntityManager, Repository } from 'typeorm';
 import { Workout } from './workoutplan.entity';
